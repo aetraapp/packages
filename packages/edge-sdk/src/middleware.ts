b7cf95d719b4eb3b2aa772eb93b136db12c9a874
Luke Bussey <luke.bussey@gmail.com>
2025-11-29 10:45:58 -0700

    Revert c21fd59

--- packages/edge-sdk/src/middleware.ts
+++ packages/edge-sdk/src/middleware.ts
@@ -17,6 +17,12 @@ export const cookieMiddleware = (options: EdgeSDKOptions) =>
   createMiddleware(async (context, next) => {
     await next();
 
+    const contentType = context.res.headers.get('content-type');
+
+    if (!contentType?.includes('text/html')) {
+      return;
+    }
+
     const { hostname } = new URL(context.req.url);
     const domain = getDomain(hostname);
 
